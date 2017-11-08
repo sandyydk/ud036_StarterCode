2a5d2d0359a1ba5c774d9af6a2212b5cf5bed795
sandyydk <sandyethadka@gmail.com>
2017-11-08 20:37:03 +0530

    Indentation made to comply with PEP8 standards

--- media.py
+++ media.py
@@ -4,10 +4,12 @@ from video import Video
 
 class Movie(Video):
     """ This class provides away to store movie related information """
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, duration):
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube, duration):
         Video.__init__(self, movie_title, duration, movie_storyline)
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """Ununsed now.Can be used to directly launch trailer of a movie"""
         webbrowser.open(self.trailer_youtube_url)
